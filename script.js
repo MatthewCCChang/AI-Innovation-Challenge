fa538bd583df7c8619711dbca24566aea11f2e60
Kainoa Nishida <kanishid@uci.edu>
2023-11-14 23:23:46 -0800

    honestly i forgot what i did

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function sendMessage() {
 
     var newMessage = document.createElement("div");
     newMessage.className = "message user-message";
-    newMessage.textContent = "You: " + message;
+    // newMessage.textContent = "You: " + message;
+    newMessage.textContent = message;
 
     chat.appendChild(newMessage);
     messageInput.value = "";
@@ -26,12 +27,12 @@ function sendMessage() {
         // Add information about the club
         response.innerHTML = `
 
-            <p>AI: Tomo No Kai is a fantastic club!</p>
+            <p>Tomo No Kai is a fantastic club! Here's a brief overview of everything I found. </p>
 
             <img src="tomo.jpeg" alt="Tomo No Kai Logo" width="100" height="100">
             <ul>
-                <li>Name: Tomo No Kai</li>
-                <li>Description: Tomo No Kai, roughly translating into 'circle of friends', was established on the campus of University of California, Irvine in 1977. We are a cultural and social club that explore the Japanese and Japanese-American cultures. We embody inclusivity and provide an environment where one can find their second home and build everlasting bonds. We strive to teach everyone who is enamored with the Japanese culture with our weekly vocabulary lessons, and our practices and performances of traditional entertainment acts which attempt to portray the Japanese community. Every week we have meetings where we update our general members with events, ongoing programs, and afterwards, we explore the city with our after events by taking the general members out to various locations and food hot spots. Furthermore, we pride ourselves in our involvement with the wider Japanese community through volunteer work and events with the Southern California Nikkei community. Please feel free to reach out to any present cabinet members with any questions or concerns! Don't forget to follow us on our Facebook, Instagram, and Snapchat to see the most current events!</li>
+                <b><li>Name: Tomo No Kai</li></b>
+                <li>Tomo No Kai, roughly translating into 'circle of friends', was established on the campus of University of California, Irvine in 1977. We are a cultural and social club that explore the Japanese and Japanese-American cultures. We embody inclusivity and provide an environment where one can find their second home and build everlasting bonds. We strive to teach everyone who is enamored with the Japanese culture with our weekly vocabulary lessons, and our practices and performances of traditional entertainment acts which attempt to portray the Japanese community. Every week we have meetings where we update our general members with events, ongoing programs, and afterwards, we explore the city with our after events by taking the general members out to various locations and food hot spots. Furthermore, we pride ourselves in our involvement with the wider Japanese community through volunteer work and events with the Southern California Nikkei community. Please feel free to reach out to any present cabinet members with any questions or concerns! Don't forget to follow us on our Facebook, Instagram, and Snapchat to see the most current events!</li>
             <li></li>
             <li><a href="https://tomonokaiuci.com/about" target="_blank">
                 Website: https://tomonokaiuci.com/about
@@ -50,7 +51,7 @@ function sendMessage() {
         // Default response for other messages
         var defaultResponse = document.createElement("div");
         defaultResponse.className = "message assistant-message";
-        defaultResponse.textContent = "AI: That's interesting! Tell me more.";
+        defaultResponse.textContent = "That's interesting! Tell me more.";
 
         chat.appendChild(defaultResponse);
     }
