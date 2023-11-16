4104065fb98c77c773032241c82373f11c2ebb3c
Matthew <deadwalker1228@gmail.com>
2023-11-16 11:18:28 -0800

    Fixed things up

--- client/script.js
+++ client/script.js
@@ -28,18 +28,16 @@ function sendMessage() {
         response.innerHTML = `
 
             <p>Tomo No Kai is a fantastic club! Here's a brief overview of everything I found. </p>
-
-            <img src="tomo.jpeg" alt="Tomo No Kai Logo" width="100" height="100">
             <ul>
-                <b><li>Name: Tomo No Kai</li></b>
+                <b><li class="title">Name: Tomo No Kai</li></b>
                 <li>Tomo No Kai, roughly translating into 'circle of friends', was established on the campus of University of California, Irvine in 1977. We are a cultural and social club that explore the Japanese and Japanese-American cultures. We embody inclusivity and provide an environment where one can find their second home and build everlasting bonds. We strive to teach everyone who is enamored with the Japanese culture with our weekly vocabulary lessons, and our practices and performances of traditional entertainment acts which attempt to portray the Japanese community. Every week we have meetings where we update our general members with events, ongoing programs, and afterwards, we explore the city with our after events by taking the general members out to various locations and food hot spots. Furthermore, we pride ourselves in our involvement with the wider Japanese community through volunteer work and events with the Southern California Nikkei community. Please feel free to reach out to any present cabinet members with any questions or concerns! Don't forget to follow us on our Facebook, Instagram, and Snapchat to see the most current events!</li>
             <li></li>
             <li><a href="https://tomonokaiuci.com/about" target="_blank">
                 Website: https://tomonokaiuci.com/about
             </a>
         </li>
-            <li><a href="https://discord.com/invite/GBUBMPu" target="_blank"><img src="discord_logo.png" alt="Discord Logo" width="20" height="20"> Discord</a> </li>
-            <li><a href="https://www.instagram.com/tomonokai.uci/" target="_blank"><img src="instagram_logo.png" alt="Instagram Logo" width="20" height="20"> Instagram</a> </li>
+            <li><a href="https://discord.com/invite/GBUBMPu" target="_blank"><img src="./img/discord.png" class = "logo" alt="Discord Logo" width="20" height="20"> Discord</a> </li>
+            <li><a href="https://www.instagram.com/tomonokai.uci/" target="_blank"><img src="./img/instagram.jpg" class = "logo" alt="Instagram Logo" width="20" height="20"> Instagram</a> </li>
             
                 <!-- Add more information as needed -->
             </ul>
@@ -63,6 +61,18 @@ function sendMessage() {
     });
 }
 
+
+window.AlltiusSDK("configure", {
+    metatadata: {
+      widgetId: "6556671fbd303fb3897c18d8",
+    },
+  });
+  
+
+(() => {const p=(l,s,a,i)=>{const n=()=>
+    {if(!s.getElementById(a)){const e=s.getElementsByTagName(i)[0],t=s.createElement(i);t.type="text/javascript",
+    t.async=!1,t.src="https://app.alltius.ai/alltiusSDK.js",t.id=a,e&&e.parentNode?e.parentNode.insertBefore(t,e):s.body.appendChild(t)}};
+    if(!l.AlltiusSDK){const e=(...t)=>{e.q.push(t)};e.q=[],l.AlltiusSDK=e,s.readyState==="complete"?n():l.addEventListener("load",n)}};p(window,document,"alltius-sdk","script");})()
 
 
 
--- design.md
+++ design.md
@@ -9,3 +9,14 @@ dark mode:
 #081955
 #FFC7C2
 #AFF4C6
+
+
+we need a authentication page asap
+
+user will be username and password
+user entity will have username, password, and antalmanc usernam as attributes (possibly another attribute that links interested club to club names)
+we should be able to save chats ( maybe set a maximum 5 )
+
+API connect with assistant ID
+React + Node + express +AWS
+
