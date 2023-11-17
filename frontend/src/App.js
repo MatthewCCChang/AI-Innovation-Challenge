e991e830914ad31c45399bb13d1eeed7856a2c58
Kainoa Nishida <kanishid@uci.edu>
2023-11-17 14:59:15 -0800

    Moved everything to frontend directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import AuthDetails from './components/AuthDetails';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route index element = {<SignIn />} />
+        <Route element = {<SignUp />} />
+        <Route element = {<AuthDetails />} />
+        <Route path='/Home' element={<Home />} />
+      </Routes>
+      </BrowserRouter>
+      
   );
 }
+/*
 
+function App() {
+    return (<BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+      </BrowserRouter>
+      );
+}
+  
+
+
+*/
 export default App;
