af3f695c2892c972da587cec6eddc924a7e4843f
Kainoa Nishida <kanishid@uci.edu>
2023-11-17 09:58:23 -0800

    Created login functionality and routes such that a successful login pushes you to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import SignIn from './components/auth/SignIn';
+import SignIn from '../../firebase-auth/src/components/auth/SignIn';
+import { Home, About, Contact } from './pages';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+    </Routes>
+    </BrowserRouter>
+    );
 }
 
 export default App;
